eb60b04d302c4b5b8b01aaa7cc08065770b10d12
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2021-06-24 19:11:19 +0100

    added block-level invalid and tamper tests

--- test_block.py
+++ test_block.py
@@ -26,6 +26,29 @@ def test_single_block():
     assert root.is_valid()
 
 
+def test_bad_block():
+    pvt1, pub1 = sign.gen_keys()
+    pvt2, pub2 = sign.gen_keys()
+    pvt3, pub3 = sign.gen_keys()
+
+    root = block.TxBlock()
+    tx = transaction.Tx(
+        inputs=[(pub1, 1),],
+        outputs=[(pub2, 100),],  # this makes the transaction invalid.
+    )
+    tx.sign(pvt1)
+    root.add_tx(tx)
+
+    tx2 = transaction.Tx(
+        inputs=[(pub3, 1),],
+        outputs=[(pub1, 1),],
+    )
+    tx2.sign(pvt3)
+    root.add_tx(tx2)
+
+    assert not root.is_valid()
+
+
 def test_block_serdes():
     pvt1, pub1 = sign.gen_keys()
     pvt2, pub2 = sign.gen_keys()
@@ -94,4 +117,50 @@ def test_multiple_blocks():
     child.add_tx(tx4)
 
     assert child.is_valid()
+    assert root.is_valid()
+
+
+def test_block_tampering():
+    pvt1, pub1 = sign.gen_keys()
+    pvt2, pub2 = sign.gen_keys()
+    pvt3, pub3 = sign.gen_keys()
+
+    # root block and its transactions.
+    root = block.TxBlock()
+    tx = transaction.Tx(
+        inputs=[(pub1, 1),],
+        outputs=[(pub2, 1),],
+    )
+    tx.sign(pvt1)
+    root.add_tx(tx)
+
+    tx2 = transaction.Tx(
+        inputs=[(pub3, 1),],
+        outputs=[(pub1, 1),],
+    )
+    tx2.sign(pvt3)
+    root.add_tx(tx2)
+
+    # child block and its transactions.
+    child = block.TxBlock(previous_block=root)
+    tx3 = transaction.Tx(
+        inputs=[(pub2, 1),],
+        outputs=[(pub3, 1),],
+    )
+    tx3.sign(pvt2)
+    child.add_tx(tx3)
+
+    tx4 = transaction.Tx(
+        inputs=[(pub1, 1),],
+        outputs=[(pub3, 1),],
+        required=[pub2],
+    )
+    tx4.sign(pvt1)
+    tx4.sign(pvt2)
+    child.add_tx(tx4)
+
+    # tamper the child by adding an extra transaction.
+    child.previous_block.add_tx(tx4)
+
+    assert not child.is_valid()
     assert root.is_valid()
